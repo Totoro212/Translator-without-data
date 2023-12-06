6d40860f95cb61fc3a8e6213332e59aa52410950
Totoro212 <146211359+Totoro212@users.noreply.github.com>
2023-12-06 16:40:28 +0500

    Add files via upload

---
+++ Переводчик/Переводчик/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Переводчик
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(451, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(451, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "uz",
+            "ru ",
+            "en",
+            "kor"});
+            this.comboBox1.Location = new System.Drawing.Point(523, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "uz",
+            "ru ",
+            "en",
+            "kor"});
+            this.comboBox2.Location = new System.Drawing.Point(523, 67);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(71, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(66, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 212);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(390, 118);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(204, 212);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Переводчик/Переводчик/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Переводчик
+{
+
+    public partial class Form1 : Form
+    {
+        string[,] dic = new string[100, 4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void DIC()
+        {
+            dic[0, 0] = "Salom"; dic[0, 1] = "Привет"; dic[0, 2] = "Hi"; dic[0, 3] = "annyeonghaseyo";
+            dic[1, 0] = "Yaxshi"; dic[1, 1] = "Хорошо"; dic[1, 2] = "Good"; dic[1, 3] = "gwaenchanh-eun";
+            dic[2, 0] = "Yomon"; dic[2, 1] = "Плохо"; dic[2, 2] = "Bad"; dic[2, 3] = "simhage";
+            dic[3, 0] = "Va"; dic[3, 1] = "И"; dic[3, 2] = "And"; dic[3, 3] = "geuligo";
+            dic[4, 0] = "Yoki"; dic[4, 1] = "Или"; dic[4, 2] = "Or"; dic[4, 3] = "ttoneun";
+            dic[5, 0] = "Kompyuter"; dic[5, 1] = "Компьютер"; dic[5, 2] = "Computer"; dic[5, 3] = "keompyuteo";
+            dic[6, 0] = "Men"; dic[6, 1] = "я"; dic[6, 2] = "I"; dic[6, 3] = "na";
+            dic[7, 0] = "Siz"; dic[7, 1] = "ты"; dic[7, 2] = "you"; dic[7, 3] = "neo";
+            dic[8, 0] = "qil"; dic[8, 1] = "делать"; dic[8, 2] = "do"; dic[8, 3] = "hada";
+            dic[9, 0] = "vaqt"; dic[9, 1] = "время"; dic[9, 2] = "time"; dic[9, 3] = "sigan";
+            dic[10, 0] = "Agar"; dic[10, 1] = "если"; dic[10, 2] = "if"; dic[10, 3] = "man-yag-e";
+            dic[11, 0] = "istayman"; dic[11, 1] = "хотеть"; dic[11, 2] = "want"; dic[11, 3] = "wonhada";
+            dic[12, 0] = "gapirish"; dic[12, 1] = "говорить"; dic[12, 2] = "said"; dic[12, 3] = "malhada";
+            dic[13, 0] = "o'ynash"; dic[13, 1] = "играть"; dic[13, 2] = "play"; dic[13, 3] = "nolda";
+            dic[14, 0] = "Yer"; dic[14, 1] = "земля"; dic[14, 2] = "land"; dic[14, 3] = "jigu";
+            dic[15, 0] = "Odamlar"; dic[15, 1] = "люди"; dic[15, 2] = "people"; dic[15, 3] = "salamdeul";
+            dic[16, 0] = "oila"; dic[16, 1] = "семья"; dic[16, 2] = "family"; dic[16, 3] = "gajog";
+            dic[17, 0] = "kun"; dic[17, 1] = "день"; dic[17, 2] = "day"; dic[17, 3] = "naj";
+            dic[18, 0] = "ovqat"; dic[18, 1] = "еда"; dic[18, 2] = "food"; dic[18, 3] = "eumsig";
+            dic[19, 0] = "suv"; dic[19, 1] = "вода"; dic[19, 2] = "water"; dic[19, 3] = "mul";
+            dic[20, 0] = "Ish"; dic[20, 1] = "работа"; dic[20, 2] = "work "; dic[20, 3] = "jig-eob";
+        }
+
+        void Selected()
+        {
+            listBox1.Items.Clear(); listBox2.Items.Clear();
+            int i = 0;
+            if (comboBox1.SelectedIndex >= 0 & comboBox2.SelectedIndex >= 0)
+                while (dic[i, comboBox1.SelectedIndex] != "" & i < 21)
+                {
+                    listBox1.Items.Add(dic[i, comboBox1.SelectedIndex]);
+                    listBox2.Items.Add(dic[i++, comboBox2.SelectedIndex]);
+                }
+        }
+
+        string TR(string S, int tr1, int tr2)
+        {
+            string[] words = S.Split(' ');
+            string t, T;
+            string sent = "";
+            string[] tr_words = new string[words.Length];
+            int num = 0, i = 0, j = 0;
+
+            while (true & dic.Length / 4 > i & j != words.Length)
+            {
+                t = "";
+                if (words[j].LastIndexOf("ning") > 0) { t = "ning"; T = words[j].Remove(words[j].LastIndexOf("ning")); }
+                else if (words[j].LastIndexOf("lar") > 0) { t = "lar"; T = words[j].Remove(words[j].LastIndexOf("lar")); }
+                else if (words[j].LastIndexOf("s ") > 0) { t = "s"; T = words[j].Remove(words[j].LastIndexOf("s")); }
+                else if (words[j].LastIndexOf("ете") > 0) { t = "ете"; T = words[j].Remove(words[j].LastIndexOf("ете")); }
+                else if (words[j].LastIndexOf("ешь") > 0) { t = "ешь"; T = words[j].Remove(words[j].LastIndexOf("ешь")); }
+                else if (words[j].LastIndexOf("nin") > 0) { t = "nin"; T = words[j].Remove(words[j].LastIndexOf("nin")); }
+                else T = words[j];
+
+                for (int y = 0; y < listBox1.Items.Count; y++)
+                {
+                    if (T.ToLower() == listBox1.Items[y].ToString().ToLower()  )
+                    {
+                        if (t == "")
+                        {
+                            tr_words[j] = dic[y, tr2] + " "; break;
+                        }
+                        else if (t == "ning")
+                        {
+                            if (tr2 == 1) { tr_words[j] = dic[y, tr2] + "ешь "; break; }
+                            if (tr2 == 2) { tr_words[j] = dic[y, tr2] + "'s "; break; }
+                            if (tr2 == 3) { tr_words[j] = dic[y, tr2] + "nin "; break; }
+
+                        }
+                        else if (t == "lar")
+                        {
+                            if (tr2 == 1) { tr_words[j] = dic[y, tr2] + "ете "; break; }
+                            if (tr2 == 2) { tr_words[j] = dic[y, tr2] + "s"; break; }
+                            if (tr2 == 3) { tr_words[j] = dic[y, tr2] + "nin "; break; }
+
+                        }
+                        else if (t == "s")
+                        {
+                            if (tr2 == 1) { tr_words[j] = dic[y, tr2] + "ете "; break; }
+                            if (tr2 == 0) { tr_words[j] = dic[y, tr2] + "lar"; break; }
+                            if (tr2 == 3) { tr_words[j] = dic[y, tr2] + "nin "; break; }
+
+                        }
+                        else if (t == "ете")
+                        {
+                            if (tr2 == 0) { tr_words[j] = dic[y, tr2] + "lar"; break; }
+                            if (tr2 == 2) { tr_words[j] = dic[y, tr2] + "s"; break; }
+                            if (tr2 == 3) { tr_words[j] = dic[y, tr2] + "nin"; break; }
+                        }
+                        else if (t == "ешь")
+                        {
+                            if (tr2 == 0) { tr_words[j] = dic[y, tr2] + "ning"; break; }
+                            if (tr2 == 2) { tr_words[j] = dic[y, tr2] + "'s"; break; }
+                            if (tr2 == 3) { tr_words[j] = dic[y, tr2] + "nin "; break; }
+                        }
+                        else if (t == "nin")
+                        {
+                            if (tr2 == 1) { tr_words[j] = dic[y, tr2] + "ете "; break; }
+                            if (tr2 == 0) { tr_words[j] = dic[y, tr2] + "ning "; break; }
+                            if (tr2 == 2) { tr_words[j] = dic[y, tr2] + "'s "; break; }
+                        }
+                    }
+                    else tr_words[j] = T;
+                }
+
+                num += 1; j++;
+                if (num == words.Length)
+                {
+                    
+                    return string.Join(" ", tr_words);
+                }
+            }
+            return "";
+        }
+
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        { Selected(); }
+        private void comboBox2_Click(object sender, EventArgs e)
+        { Selected(); }
+        private void Form1_Load(object sender, EventArgs e)
+        { DIC(); }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = TR(textBox1.Text, comboBox1.SelectedIndex, comboBox2.SelectedIndex);
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.Items[listBox1.SelectedIndex].ToString();
+            textBox2.Text = TR(textBox1.Text, comboBox1.SelectedIndex, comboBox2.SelectedIndex);
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            textBox2.Text = listBox2.Items[listBox2.SelectedIndex].ToString();
+            textBox1.Text = TR(textBox2.Text, comboBox2.SelectedIndex, comboBox1.SelectedIndex);
+        }
+    }
+}
